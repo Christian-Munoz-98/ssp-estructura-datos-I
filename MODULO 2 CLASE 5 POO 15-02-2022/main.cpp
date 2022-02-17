9c4e4580131e7e0b9c03e0f0d1b75819f0a2ca68
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-02-16 23:40:09 -0600

    Menu terminado

--- MODULO 2 CLASE 5 POO 15-02-2022/main.cpp
+++ MODULO 2 CLASE 5 POO 15-02-2022/main.cpp
@@ -1,29 +1,62 @@
 #include <iostream>
+#include <string>
 #include "Triangulo.h"
 #include "Cuadrado.h"
 #include "Rectangulo.h"
+#include "Poligono.h"
 
 using namespace std;
 
+/*string numero = "125879689077854654";
+cout<< d_input(numero) << endl;
+cout<< stod(numero)*2 <<endl;*/
+
+
+bool d_input(string);
+bool i_input(string);
+
 int main(){
-    double base = 0,altura;
-    int opc=0;
-    Rectangulo r;
-    do{
-        system("cls");
-        cout << "Ingrese la base: ";
-        cin >> base;
-        cout << "Ingrese la altura: ";
-        cin >> altura;      
+    string numero = "123A456789";
+    cout<< i_input(numero) << endl;
+    cout<< stoi(numero)*2 <<endl;
 
-        r.setBase(base);
-        r.setAltura(altura);       
-        r.calculaArea();
-        cout << "Area: " << r.getArea() << endl;
+}
 
-        cout<<"Continuar 1.Si 2.No"<<endl;
-        cin>>opc;
-    }while(opc!=2);
-    return 0;
+bool d_input(string input){
+    int length = input.length();
+    int point_counter = 0;
+    for(int i=0; i<length;i++){
+
+        cout << input[i] << endl;
+
+        if(!isdigit(input[i])){
+            if(input[i]=='.'){
+                point_counter++;
+                cout << "Puntos: " << point_counter << endl;
+                if(point_counter>1){
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+
+    }
+    return true;
+}
+
+bool i_input(string input){
+    int length = input.length();
+    for(int i=0; i<length;i++){
+
+        cout << input[i] << endl;
+
+        if(!isdigit(input[i])){
+            return false;
+        }
+
+    }
+    return true;
 }
 
