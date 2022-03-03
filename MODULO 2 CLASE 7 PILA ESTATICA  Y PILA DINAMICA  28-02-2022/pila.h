073ed9757adea9d62173b8bab3e04334f52f80ed
Christian Muñoz <chgeovany.15@gmail.com>
2022-03-02 22:37:46 -0600

    Pila estatica

---
+++ MODULO 2 CLASE 7 PILA ESTATICA  Y PILA DINAMICA  28-02-2022/pila.h
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<stdlib.h>
+#include<windows.h>
+using namespace std;
+
+class pila{
+private:
+    int datos[10],tope;
+
+public:		
+	pila();
+	void push();
+	void pop();
+	int  vacia();
+	int llena();
+	void mostrar_tope();
+	void mostrar_pila();
+	void gotoxy(int,int);
+		
+		
+};
+
+pila::pila(){
+	tope=-1;
+}
+
+void pila::push(){
+	int x;
+	if(llena()==0){
+		cout<<"ingresa numero"<<endl;
+		cin>>x;
+		tope ++;
+	    datos[tope]=x;	
+	}	
+}
+
+void pila::pop(){
+	if(vacia()==0){
+		datos[tope ]= 0;
+		tope --;
+	}
+	
+}
+void pila::mostrar_tope(){
+	if(vacia()==0){
+		system("cls");
+		cout<<"El tope es:  "<<datos[tope ]<<endl;
+		system("pause");
+	}
+}
+
+void pila::mostrar_pila(){
+	int ayuda,i;
+	if(vacia()==0){
+		system("cls");
+	   	printf("\n\n\t\t%c%c%c%c%c  \n",201,205,205,205,187);
+	    for(i=tope;i>=0;i--){
+		     printf("\t\t%c ",186,205);
+		    cout<<datos[i]; printf(" %c\n",186);
+		    printf("\t\t%c%c%c%c%c\n",204,205,205,205,185);
+		    
+		}
+			//system("pause");
+	}
+	
+  }
+
+int  pila::vacia(){
+	if(tope==-1){
+		system("cls");
+		cout<<"LA PILA ESTA VACIA"<<endl;
+		system("pause");
+		
+		return 1;
+	} else{
+		return 0;
+	}	
+}
+int  pila::llena(){
+	if(tope==9){
+		system("cls");
+		cout<<"LA PILA ESTA LLENA "<<endl;
+		system("pause");
+	     return 1;
+	
+	}
+	else{
+			return 0;
+	}
+}
+
+void pila:: gotoxy(int x,int y){  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos);  
+ }
